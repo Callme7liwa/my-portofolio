b1c49090b2d20aee1ef08968b81baa56778a7972
Callme7liwa <ayoub.seddiki@usmba.ac.ma>
2023-03-06 19:45:21 +0100

    first commit

---
+++ src/Components/SideBare.js
@@ -0,0 +1,8 @@
+
+const SideBare = () => {
+    return (
+        <div className="side-bare-container">
+            
+        </div>
+    )
+}
---
+++ src/Images/index.js
@@ -0,0 +1,34 @@
+import profile_image from "./ayoub2.jpg";
+import image_principal from "./image_principal.png";
+import git_hub from "./github.png";
+import post_man from "./postMan.png";
+import react_js from "./reactjs.png";
+import birthday_icon from "./birthday_icon.png"
+import location_icon from "./location_icon.png";
+import nationality_icon from "./nationality_icon.png";
+import city_icon from "./city_icon.png";
+import formation_icon from "./formation_icon.png"
+import logo_fst from "./logo_fst.png"
+import ensias_logo from "./ensias_logo.png";
+import experience_icon from "./experiences_ico.png"
+import elr12_logo from "./elr12_logo.jpeg";
+import project_icon from "./projects_icon.png";
+import web_dev_1 from "./web_dev.png"
+import web_dev_2 from "./web_dev_1.png";
+import web_dev_3 from "./web_dev_2.png";
+import web_dev_4 from "./web_dev_3.png";
+import web_dev_5 from "./web_dev_4.png";
+import web_dev_6 from "./web_dev_5.png";
+import web_dev_7 from "./web_dev_6.png";
+import mobil_dev_1 from "./mobil_dev.jpg";
+import project_name_icon from "./project_name_icon.png"
+import flash_back_icon from "./flashBack_icon.png"
+import period_icon from "./period_icon.png"
+
+export default {
+    profile_image,image_principal,git_hub,post_man,react_js,
+    birthday_icon,location_icon,nationality_icon,city_icon,
+    formation_icon,logo_fst,ensias_logo,experience_icon,elr12_logo,
+    project_icon,web_dev_1,web_dev_2,web_dev_3,web_dev_4,project_name_icon,flash_back_icon,
+    period_icon,web_dev_5,web_dev_6,web_dev_7,mobil_dev_1
+}
